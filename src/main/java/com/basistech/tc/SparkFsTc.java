7c1b74099b8c66e0d5fc6ecf5e8a5f928378e5ae
Benson Margulies <benson@basistech.com>
2016-08-02 14:54:06 -0400

    Test case it is.

--- src/main/java/com/basistech/tc/SparkFsTc.java
+++ src/main/java/com/basistech/tc/SparkFsTc.java
@@ -26,6 +26,8 @@ import java.net.URI;
 import java.nio.file.FileSystem;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.Iterator;
 
 public final class SparkFsTc {
     private SparkFsTc() {
@@ -36,13 +38,14 @@ public final class SparkFsTc {
         SparkConf conf = new SparkConf().setAppName("File System Test Case");
         JavaSparkContext sc = new JavaSparkContext(conf);
 
-        JavaRDD<String> logData = sc.textFile("dummy.txt");
+        JavaRDD<String> logData = sc.parallelize(Collections.singletonList("foo"));
+        System.out.println(logData.getNumPartitions());
         logData.mapPartitions(itr -> {
-            FileSystem fs = Jimfs.newFileSystem("test");
+            FileSystem fs = Jimfs.newFileSystem();
             Path path = fs.getPath("/root");
             URI uri = path.toUri();
             Paths.get(uri); // expect this to go splat.
             return null;
-        });
+        }).collect();
     }
 }
